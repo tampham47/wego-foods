dec4fd2f434a6411b6811cf1403bc10e854302db
Tam Pham <tampham47@live.com>
2024-01-29 20:59:42 +0700

    feat: add styled-components to replace the default one

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ScContainer } from "./components/System";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <ScContainer>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Nulla quis elit eiusmod cupidatat sint sit reprehenderit reprehenderit
+          proident commodo ullamco ea. Quis laborum sit consectetur dolor
+          deserunt exercitation pariatur. Incididunt sit labore et veniam in
+          elit duis incididunt sit id aliqua laborum proident. Fugiat ea dolor
+          ad anim in in anim occaecat ut dolore. Dolore dolore pariatur ullamco
+          magna velit esse cupidatat. Occaecat tempor occaecat deserunt dolor ea
+          non aliquip laborum. Exercitation voluptate proident minim cillum
+          commodo.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </ScContainer>
     </div>
   );
 }
---
+++ src/components/System.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ScContainer = styled.div`
+  max-width: 975px;
+  margin-left: auto;
+  margin-right: auto;
+`;
